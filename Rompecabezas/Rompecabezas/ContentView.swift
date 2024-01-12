cdb587a8c6fc3fff99d0f6dea3766c61f887da1a
AlonsoM207 <alonsoabimaelmoralesreyna@gmail.com>
2024-01-12 11:37:30 -0600

    Prueba

---
+++ Rompecabezas/Rompecabezas/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Rompecabezas
+//
+//  Created by Ivonne Reyna on 12/01/24.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+            LaunchScreenView()
+        }
+        .navigationViewStyle(.stack)
+
+    }// body
+}// View
---
+++ Rompecabezas/Rompecabezas/RompecabezasApp.swift
@@ -0,0 +1,23 @@
+//
+//  RompecabezasApp.swift
+//  Rompecabezas
+//
+//  Created by Ivonne Reyna on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_Equipo1App: App {
+    
+    @StateObject private var predictionStatus = PredictionStatus()
+    //@StateObject private var predictionStatus2 = PredictionStatus2()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(predictionStatus)
+                //.environmentObject(predictionStatus2)
+        }
+    }
+}
